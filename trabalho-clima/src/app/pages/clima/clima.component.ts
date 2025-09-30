75d46dc0a6ded0d3c3d72a2a67b692b1ce1d171d
rafaelpilonetto <rafaelpilonetto59@gmail.com>
2025-09-30 20:57:24 -0300

    commit1

--- trabalho-clima/src/app/pages/clima/clima.component.ts
+++ trabalho-clima/src/app/pages/clima/clima.component.ts
@@ -2,22 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OpenWeatherService } from '../service/open-weather.service';
 import { weatherResponse } from '../models/weather-responde.model';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-clima',
   templateUrl: './clima.component.html',
   styleUrls: ['./clima.component.scss']
 })
-export class ClimaComponent implements OnInit {
+export class ClimaComponent  {
 
   navegaodr = inject(Router);
   openWeatherService = inject(OpenWeatherService);
-  dadosClima: weatherResponse | null = null;
+  dadosClima = toSignal<weatherResponse | null>(this.openWeatherService.buscarInfoClimaCidadeAtual());
   constructor() { }
-
-  ngOnInit() {
-    this.openWeatherService.buscarInfoClimaCidadeAtual();
-  }
 
   navegarParaTelaTelaPesquisa() {
     this.navegaodr.navigate(['pesquisa']);
